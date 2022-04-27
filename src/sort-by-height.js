cadccad6fd1855e96c5f5bb4064614169661fa03
Siarhei Prymakovich <prima.psa@gmail.com>
2022-04-27 14:38:32 +0300

    feat: imlement sort-by-height function

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const positon = arr.map((el, index) => {if (el === -1) { return index;}}).filter((el) => isFinite(el));
+  const sorted = arr.filter((el, indx) => el!==-1).sort((a, b) => a - b);
+  positon.forEach((el) => sorted.splice(el, 0, -1))  
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
