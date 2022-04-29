df9137bf2674ffd7d923d56ca348704024b64499
Siarhei Prymakovich <prima.psa@gmail.com>
2022-04-29 17:22:23 +0300

    feat: implement mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -11,9 +11,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
+ *    0      1      2
+ * 0 [true, false, false],
+ * 1 [false, true, false],
+ * 2 [false, false, false]
  * ]
  *
  * The result should be following:
@@ -23,9 +24,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let setuped = [...matrix];
+  let combinations = [];
+  for (let is = 0; is < matrix.length; is++) {
+    for (let js = 0; js < matrix[is].length; js++) {
+      let cell = matrix[is][js];
+      if (cell === true) {        
+        getCombinations(is, js, setuped.length);
+      }
+      setuped[is][js] = 0;
+    }
+  }
+
+  function getCombinations(x, y, size) {
+    const start = -1;
+    const end = 1;
+    for (let ig = start; ig <= end; ig++) {
+      for (let jg = start; jg <= end; jg++) {
+        if (!(ig == 0 && jg == 0)) {
+          let offsetX = x - ig;
+          let offsetY = y - jg;
+          if (
+            offsetX >= 0 &&
+            offsetY >= 0 &&
+            offsetX < size &&
+            offsetY < size
+          ) {
+            combinations.push([offsetX, offsetY]);
+          }
+        }
+      }
+    }
+  }
+
+  function updateCell(arr) {
+
+    arr.forEach((el) => {
+      let ic = el[0];
+      let jc = el[1];      
+      setuped[ic][jc] += 1;      
+    });
+  }
+  
+  updateCell(combinations);
+
+  return setuped;
 }
 
 module.exports = {
